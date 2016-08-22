ff1fa86ac67cf3292b86b941f576dfac15009cc7
sophienave <sophien@amdocs.com>
2016-08-22 11:17:41 +0300

    add app folder + startServer and update dependencies for express and body-parser

---
+++ startServer.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var server = express();
+server.use(express.static('app'));
+server.use(bodyParser.json());
+
+var createdServer = server.listen(3000, function () {
+
+  var host = createdServer.address().address;
+  var port = createdServer.address().port;
+  var address;
+  if(host === "::"){
+    address = "http://localhost:" + port;
+  }
+  else{
+  	address = "http://" + host + ':' + port;
+  }
+
+  console.log('projectList app listening at ' + address);
+
+});
