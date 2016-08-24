f086e0bf998963507025aabb340a410ea891fe33
sophienave <sophien@amdocs.com>
2016-08-24 20:14:17 +0300

    add get function using promise and render the data using react

--- app/main.jsx
+++ app/main.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-        <h1>Hello, world!</h1>,
-        document.getElementById('main')
-      );
+
+
+get('/projects').then(function(projects){
+	
+	console.log("projects "+ projects);
+
+	var projectsDOM = projects.map(function(project){
+		return <div>{project.id}</div>;
+	});
+	ReactDOM.render(<div>{projectsDOM}</div>,document.getElementById('main'));
+});
+
+function get(url){
+	var promise =  new Promise(function(resolve, reject) {
+		var request = new XMLHttpRequest();
+		request.open('GET', url, true);
+
+		request.onload = function() {
+	  	if (request.status >= 200 && request.status < 400) {
+		    var data = JSON.parse(request.responseText);
+		    resolve(data);
+		  } else {
+		    reject();
+		  }
+		};
+
+		request.onerror = function() {
+		  reject();
+		};
+
+		request.send();
+	});
+	return promise;
+}
+
+
